15792ca8aaabf6effa90ee39641c9ab1ddc9a90e
Kevin McGonigal <kevin.mcgonigal@gmail.com>
2019-08-01 21:45:37 -0600

    more setup

--- tests/basic_db_test.py
+++ tests/basic_db_test.py
@@ -1,7 +1,25 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models.all_models import User
 
 # connect_string = 'sqlite:///:memory:'
 connect_string = 'mysql+pymysql://root:@localhost/sample_db_local'
-
 engine = create_engine(connect_string, echo=True)
-print(engine)
+
+SESSION = sessionmaker(bind=engine)
+session = SESSION()
+
+
+def create_user_direct():
+    user = User(
+        first_name='Kevin',
+        last_name='McGonigal',
+        nickname='Mr. Stark'
+    )
+
+    session.add(user)
+    session.commit()
+
+
+create_user_direct()
