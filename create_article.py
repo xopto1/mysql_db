09743821dfaeaa74258fc984511d003ba5007c65
Kevin McGonigal <kevin.mcgonigal@gmail.com>
2019-08-11 15:40:36 -0600

    checkout create_article.py before anything

--- create_article.py
+++ create_article.py
@@ -1,17 +1,40 @@
-from models.all_models import Article
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_continuum import make_versioned
+
 from utils.database import create_db_session, connect_string
 
+Base = declarative_base()
+make_versioned(user_cls=None)
 session = create_db_session(connect_string)
 
 
-def create_record():
-    article = Article(
-        name='Article One',
-        content='Blah. Yap, yap',
-    )
+class Article(Base):
+    __versioned__ = {}
+    __tablename__ = 'article'
 
-    session.add(article)
-    session.commit()
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    name = sa.Column(sa.Unicode(255))
+    content = sa.Column(sa.UnicodeText)
 
 
-create_record()
+article = Article(name='Some article', content='Some content')
+session.add(article)
+session.commit()
+
+# article has now one version stored in database
+article.versions[0].name
+# 'Some article'
+
+article.name = 'Updated name'
+session.commit()
+
+article.versions[1].name
+# 'Updated name'
+
+
+# lets revert back to first version
+article.versions[0].revert()
+
+article.name
+# 'Some article'
