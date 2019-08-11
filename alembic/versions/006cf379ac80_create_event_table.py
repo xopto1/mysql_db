91c5f3a9a32839d9e43cbfb569f2b9e739f93cbd
Kevin McGonigal <kevin.mcgonigal@gmail.com>
2019-08-11 15:34:07 -0600

    about to clean up and start over

--- alembic/versions/006cf379ac80_create_event_table.py
+++ alembic/versions/006cf379ac80_create_event_table.py
@@ -8,7 +8,6 @@ Create Date: 2019-08-11 14:57:49.846211
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = '006cf379ac80'
 down_revision = 'a34f924503cd'
@@ -20,7 +19,7 @@ def upgrade():
     op.create_table(
         'events',
         sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
-        sa.Column('name', sa.String()),
+        sa.Column('name', sa.String(255)),
         sa.Column('start_time', sa.DateTime, nullable=False),
         sa.Column('end_time', sa.DateTime, nullable=False),
         sa.Column('description', sa.Text),
--- models/events.py
+++ models/events.py
@@ -11,7 +11,7 @@ Base = declarative_base()
 class Event(Base):
     __tablename__ = 'events'
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
-    name = sa.Column(sa.String)
+    name = sa.Column(sa.String())
     start_time = sa.Column(sa.DateTime, nullable=False)
     end_time = sa.Column(sa.DateTime, nullable=False)
     description = sa.Column(sa.Text)
