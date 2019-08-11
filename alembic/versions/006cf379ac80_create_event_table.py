a8049c35cd490dffabcdc1702a7465a32551e226
Kevin McGonigal <kevin.mcgonigal@gmail.com>
2019-08-11 15:07:52 -0600

    starting event creation

---
+++ alembic/versions/006cf379ac80_create_event_table.py
@@ -0,0 +1,36 @@
+"""create event table
+
+Revision ID: 006cf379ac80
+Revises: a34f924503cd
+Create Date: 2019-08-11 14:57:49.846211
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '006cf379ac80'
+down_revision = 'a34f924503cd'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'events',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('name', sa.String()),
+        sa.Column('start_time', sa.DateTime, nullable=False),
+        sa.Column('end_time', sa.DateTime, nullable=False),
+        sa.Column('description', sa.Text),
+        sa.Column('schedule_published_on', sa.DateTime)
+    )
+
+
+
+    schedule_published_on = db.Column(db.DateTime)
+
+
+def downgrade():
+    pass
---
+++ models/events.py
@@ -0,0 +1,18 @@
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+# Needed for continuum
+from sqlalchemy_continuum import make_versioned
+
+# Needed for continuum
+make_versioned(user_cls=None)
+Base = declarative_base()
+
+
+class Event(Base):
+    __tablename__ = 'events'
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    name = sa.Column(sa.String)
+    start_time = sa.Column(sa.DateTime, nullable=False)
+    end_time = sa.Column(sa.DateTime, nullable=False)
+    description = sa.Column(sa.Text)
+    schedule_published_on = sa.Column(sa.DateTime)
