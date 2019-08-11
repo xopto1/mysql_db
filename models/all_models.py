c19711402ee3e817a35118a32d116a4a74e8d5e4
Kevin McGonigal <kevin.mcgonigal@gmail.com>
2019-08-11 14:26:44 -0600

    before first test, about to backup tables:

--- models/all_models.py
+++ models/all_models.py
@@ -1,10 +1,17 @@
+import sqlalchemy as sa
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+# Needed for continuum
+from sqlalchemy_continuum import make_versioned
 
+# Needed for continuum
+make_versioned(user_cls=None)
 Base = declarative_base()
 
 
 class User(Base):
+    # Needed for continuum
+    __versioned__ = {}
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
@@ -15,3 +22,5 @@ class User(Base):
         return "<User(first_name='%s', last_name='%s', nickname='%s')>" % (
                             self.full_name, self.last_name, self.nickname)
 
+# Needed for Continuum
+sa.orm.configure_mappers()
